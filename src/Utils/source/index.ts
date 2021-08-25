186b481defafcc4b96e3298e01ff55ceddf52572
Tim Crooker <timothycrooker@gmail.com>
2021-08-25 13:02:22 -0400

    linted

--- src/Utils/source/index.ts
+++ src/Utils/source/index.ts
@@ -14,43 +14,43 @@ import { GetSourceFn } from './source'
  * takes CLI source input for custom plugins source and returns the path to the directory
  */
 export const getSource: GetSourceFn = async (source) => {
-  /**
-   * Replace path if default
-   */
-  const sourceSpinner = ora(
-    `Checking provided source ${chalk.bold`"${source}"`}`
-  )
-  sourceSpinner.start()
-
-  const sourcePath =
-    source ?? 'https://github.com/BotSeaio/Perfect-Boilerplate-Plugins.git'
-
-  const PathExists = await FSHelper.PathExists(sourcePath)
-  if (PathExists) {
     /**
-     * check local path
+     * Replace path if default
      */
-    sourceSpinner.succeed('Found local source.')
-    return { path: sourcePath }
-  } else {
-    /**
-     * Check repo exists
-     * clone and return path if exists
-     */
-    sourceSpinner.text = 'Checking remote source...'
-    const repoStatus = await GitHelper.RepoExists(sourcePath)
-    if (repoStatus.exists === true) {
-      sourceSpinner.text = 'Remote source found. Cloning...'
-      const cloneResponse = await GitHelper.CloneAndGetPath(sourcePath)
-      if (cloneResponse) {
-        sourceSpinner.succeed('Cloned remote source successfully.')
-        return { path: cloneResponse + '/src' }
-      }
-      sourceSpinner.fail('Could not retrieve source repository.')
-      return { error: 'Could not retrieve source repository.' }
+    const sourceSpinner = ora(
+        `Checking provided source ${chalk.bold`"${source}"`}`
+    )
+    sourceSpinner.start()
+
+    const sourcePath =
+        source ?? 'https://github.com/BotSeaio/Perfect-Boilerplate-Plugins.git'
+
+    const PathExists = await FSHelper.PathExists(sourcePath)
+    if (PathExists) {
+        /**
+         * check local path
+         */
+        sourceSpinner.succeed('Found local source.')
+        return { path: sourcePath }
     } else {
-      sourceSpinner.fail('Could not found source repository.')
-      return { error: repoStatus.error }
+        /**
+         * Check repo exists
+         * clone and return path if exists
+         */
+        sourceSpinner.text = 'Checking remote source...'
+        const repoStatus = await GitHelper.RepoExists(sourcePath)
+        if (repoStatus.exists === true) {
+            sourceSpinner.text = 'Remote source found. Cloning...'
+            const cloneResponse = await GitHelper.CloneAndGetPath(sourcePath)
+            if (cloneResponse) {
+                sourceSpinner.succeed('Cloned remote source successfully.')
+                return { path: cloneResponse + '/src' }
+            }
+            sourceSpinner.fail('Could not retrieve source repository.')
+            return { error: 'Could not retrieve source repository.' }
+        } else {
+            sourceSpinner.fail('Could not found source repository.')
+            return { error: repoStatus.error }
+        }
     }
-  }
 }
