28a074ecfccf3e57fd53306baceabba36be102fb
Tim Crooker <timothycrooker@gmail.com>
2021-09-03 13:32:55 -0400

    spaces to tabs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,37 @@
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    extends: [
-        'standard',
-        'eslint:recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:import/errors',
-        'plugin:import/warnings',
-        'plugin:import/typescript',
-        'plugin:promise/recommended',
-        'prettier',
-    ],
-    parserOptions: {
-        ecmaVersion: 2015,
-        sourceType: 'module',
-    },
-    plugins: ['prettier', '@typescript-eslint'],
-    rules: {
-        // 'comma-dangle': ['error', 'always-multiline'],
-        // 'no-empty-pattern': ['off'],
-        // 'no-undef': ['error'],
-        'no-var': ['error'],
-        // 'object-curly-spacing': ['error', 'always'],
-        // indent: ['off'],
-        'prettier/prettier': [
-            'warn',
-            {
-                endOfLine: 'auto',
-                singleQuote: true,
-                semi: false,
-                printWidth: 80,
-                tabWidth: 4,
-            },
-        ],
-    },
-    env: {
-        // change as necessary
-        node: true,
-    },
+	root: true,
+	parser: '@typescript-eslint/parser',
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/eslint-recommended',
+		'plugin:@typescript-eslint/recommended',
+		// 'plugin:import/errors',
+		// 'plugin:import/warnings',
+		// 'plugin:import/typescript',
+		// 'plugin:promise/recommended',
+		'prettier',
+	],
+	parserOptions: {
+		ecmaVersion: 2015,
+		sourceType: 'module',
+	},
+	plugins: ['prettier', '@typescript-eslint'],
+	rules: {
+		'no-var': ['error'],
+		'prettier/prettier': [
+			'warn',
+			{
+				endOfLine: 'auto',
+				singleQuote: true,
+				semi: false,
+				printWidth: 80,
+				tabWidth: 4,
+				useTabs: true,
+			},
+		],
+	},
+	env: {
+		// change as necessary
+		node: true,
+	},
 }
--- src/cli.ts
+++ src/cli.ts
@@ -1,4 +1,4 @@
-import { FSHelper, getPackChoicesFromDir, getSource, runSao } from '@Utils'
+import { getPackChoicesFromDir, getSource, runSao } from '@Utils'
 import commander from 'commander'
 import path from 'path'
 import prompts from 'prompts'
@@ -10,193 +10,198 @@ import currentProject from '../package.json'
 const generator = path.resolve(__dirname, './')
 
 const cli = async (): Promise<void> => {
-    /**
-     * Get target project-directory
-     */
-    const program = commander
-        .name(currentProject.name)
-        .version(currentProject.version)
-        .arguments('<project-directory>')
-        .usage(`${chalk.green('<project-directory>')} [options]`)
-        .description(currentProject.description)
-        .option(
-            '-s, --source <source-path>',
-            'specify a custom source of plugins'
-        )
-        .option('-d, --debug', 'run the program in debug mode')
-        .parse(process.argv)
+	/**
+	 * Get target project-directory
+	 */
+	const program = commander
+		.name(currentProject.name)
+		.version(currentProject.version)
+		.arguments('<project-directory>')
+		.usage(`${chalk.green('<project-directory>')} [options]`)
+		.description(currentProject.description)
+		.option(
+			'-s, --source <source-path>',
+			'specify a custom source of plugins'
+		)
+		.option('-d, --debug', 'run the program in debug mode')
+		.parse(process.argv)
 
-    const [projectDir] = program.args
+	const [projectDir] = program.args
 
-    console.log(projectDir)
-    // Check target project-directory exists
-    if (projectDir === undefined) {
-        console.error('No specified project directory')
-        process.exit(1)
-    }
+	console.log(projectDir)
+	// Check target project-directory exists
+	if (projectDir === undefined) {
+		console.error('No specified project directory')
+		process.exit(1)
+	}
 
-    /**
-     * Get the plugin source directory
-     */
+	/**
+	 * Get the plugin source directory
+	 */
 
-    const source = await getSource(program.source)
+	const source = await getSource(program.source)
 
-    let { path: sourcePath } = source
-    const { error: sourceError } = source
+	let { path: sourcePath } = source
+	const { error: sourceError } = source
 
-    // error when supplied source doesn't exist
-    if (sourceError) {
-        console.error(`${chalk.bold`${sourceError}`}`)
-        console.log('Source can be a remote git repository or a local path.')
-        console.log()
-        console.log('You provided:')
-        console.log(`${chalk.blueBright(program.source)}`)
-        process.exit(1)
-    }
+	// error when supplied source doesn't exist
+	if (sourceError) {
+		console.error(`${chalk.bold`${sourceError}`}`)
+		console.log('Source can be a remote git repository or a local path.')
+		console.log()
+		console.log('You provided:')
+		console.log(`${chalk.blueBright(program.source)}`)
+		process.exit(1)
+	}
 
-    /**
-     * Verify plugin packs are valid and load them from supplied directory
-     */
+	/**
+	 * Verify plugin packs are valid and load them from supplied directory
+	 */
 
-    const packTypes = await getPackChoicesFromDir(sourcePath as string, true)
+	const packTypes = await getPackChoicesFromDir(sourcePath as string, true)
 
-    /**
-     * User selects app type to build
-     */
+	/**
+	 * User selects app type to build
+	 */
 
-    const { projectType } = await prompts({
-        type: 'select',
-        name: 'projectType',
-        message: 'Select your project type',
-        choices: packTypes,
-    })
+	const { projectType } = await prompts({
+		type: 'select',
+		name: 'projectType',
+		message: 'Select your project type',
+		choices: packTypes,
+	})
 
-    sourcePath = `${sourcePath}/${projectType}`
+	sourcePath = `${sourcePath}/${projectType}`
 
-    /**
-     * Set up SAO heiarchy for CUSTOM stacks
-     */
+	/**
+	 * Set up SAO heiarchy for CUSTOM stacks
+	 */
 
-    const saoInstances: SAO[] = []
+	const saoInstances: SAO[] = []
 
-    if (projectType === 'Custom') {
-        const stackChoices = await getPackChoicesFromDir(sourcePath, true)
+	if (projectType === 'Custom') {
+		const stackChoices = await getPackChoicesFromDir(sourcePath, true)
 
-        const { projectStack } = await prompts({
-            type: 'select',
-            name: 'projectStack',
-            message: 'Select the levels of the stack',
-            choices: stackChoices,
-        })
+		const { projectStack } = await prompts({
+			type: 'select',
+			name: 'projectStack',
+			message: 'Select the levels of the stack',
+			choices: stackChoices,
+		})
 
-        let frontendOutDir = projectDir
-        let backendOutDir = projectDir
+		let frontendOutDir = projectDir
+		let backendOutDir = projectDir
 
-        if (projectStack === 'fullstack') {
-            const fullstackSrcPath = `${sourcePath}/${projectStack}`
-            saoInstances.push(
-                new SAO({
-                    generator,
-                    outDir: projectDir,
-                    logLevel: program.debug ? 4 : 1,
-                    appName: projectDir,
-                    extras: {
-                        debug: !!program.debug,
-                        paths: {
-                            sourcePath: fullstackSrcPath,
-                        },
-                    },
-                } as Options)
-            )
-            frontendOutDir = `${projectDir}/client`
-            backendOutDir = `${projectDir}/server`
-        }
-        if (projectStack === 'frontend' || projectStack === 'fullstack') {
-            let frontendSrcPath = `${sourcePath}/frontend`
-            const frontendChoices = await getPackChoicesFromDir(frontendSrcPath)
+		if (projectStack === 'fullstack') {
+			const fullstackSrcPath = `${sourcePath}/${projectStack}`
+			saoInstances.push(
+				new SAO({
+					generator,
+					outDir: projectDir,
+					logLevel: program.debug ? 4 : 1,
+					appName: projectDir,
+					extras: {
+						debug: !!program.debug,
+						paths: {
+							sourcePath: fullstackSrcPath,
+						},
+					},
+				} as Options)
+			)
+			frontendOutDir = `${projectDir}/client`
+			backendOutDir = `${projectDir}/server`
+		}
+		if (projectStack === 'frontend' || projectStack === 'fullstack') {
+			let frontendSrcPath = `${sourcePath}/frontend`
+			const frontendChoices = await getPackChoicesFromDir(frontendSrcPath)
 
-            const { frontendType } = await prompts({
-                type: 'select',
-                name: 'frontendType',
-                message: 'Select your front-end framework',
-                choices: frontendChoices,
-            })
+			const { frontendType } = await prompts({
+				type: 'select',
+				name: 'frontendType',
+				message: 'Select your front-end framework',
+				choices: frontendChoices,
+			})
 
-            frontendSrcPath = `${frontendSrcPath}/${frontendType}`
+			frontendSrcPath = `${frontendSrcPath}/${frontendType}`
 
-            saoInstances.push(
-                new SAO({
-                    generator,
-                    outDir: frontendOutDir,
-                    logLevel: program.debug ? 4 : 1,
-                    appName: projectDir,
-                    extras: {
-                        debug: !!program.debug,
-                        paths: {
-                            sourcePath: frontendSrcPath,
-                        },
-                    },
-                    answers: {
-                        name: 'Client',
-                    },
-                } as Options)
-            )
-        }
-        if (projectStack === 'backend' || projectStack === 'fullstack') {
-            let backendSrcPath = `${sourcePath}/backend`
-            const backendChoices = await getPackChoicesFromDir(backendSrcPath)
+			saoInstances.push(
+				new SAO({
+					generator,
+					outDir: frontendOutDir,
+					logLevel: program.debug ? 4 : 1,
+					appName: projectDir,
+					extras: {
+						debug: !!program.debug,
+						paths: {
+							sourcePath: frontendSrcPath,
+						},
+					},
+					answers: {
+						name: 'Client',
+					},
+				} as Options)
+			)
+		}
+		if (projectStack === 'backend' || projectStack === 'fullstack') {
+			let backendSrcPath = `${sourcePath}/backend`
+			const backendChoices = await getPackChoicesFromDir(backendSrcPath)
 
-            const { backendType } = await prompts({
-                type: 'select',
-                name: 'backendType',
-                message: 'Select your back-end framework',
-                choices: backendChoices,
-            })
+			const { backendType } = await prompts({
+				type: 'select',
+				name: 'backendType',
+				message: 'Select your back-end framework',
+				choices: backendChoices,
+			})
 
-            backendSrcPath = `${backendSrcPath}/${backendType}`
+			backendSrcPath = `${backendSrcPath}/${backendType}`
 
-            saoInstances.push(
-                new SAO({
-                    generator,
-                    outDir: backendOutDir,
-                    logLevel: program.debug ? 4 : 1,
-                    appName: projectDir,
-                    extras: {
-                        debug: !!program.debug,
-                        paths: {
-                            sourcePath: backendSrcPath,
-                        },
-                    },
-                    answers: {
-                        name: 'Server',
-                    },
-                } as Options)
-            )
-        }
-    } else {
-        saoInstances.push(
-            new SAO({
-                generator,
-                outDir: projectDir,
-                logLevel: program.debug ? 4 : 1,
-                appName: projectDir,
-                extras: {
-                    debug: !!program.debug,
-                    paths: {
-                        sourcePath,
-                    },
-                },
-            } as Options)
-        )
-    }
+			saoInstances.push(
+				new SAO({
+					generator,
+					outDir: backendOutDir,
+					logLevel: program.debug ? 4 : 1,
+					appName: projectDir,
+					extras: {
+						debug: !!program.debug,
+						paths: {
+							sourcePath: backendSrcPath,
+						},
+					},
+					answers: {
+						name: 'Server',
+					},
+				} as Options)
+			)
+		}
+	} else {
+		saoInstances.push(
+			new SAO({
+				outDir: projectDir,
+				debug: program.debug,
+				quiet: !program.debug,
+				generator,
+				// TODO add mock logic in cli
+				mock: false,
+				// TODO add logic to provide mock answers for the plugin dev environment
+				answers: false,
+				appName: projectDir,
+				extras: {
+					debug: !!program.debug,
+					paths: {
+						sourcePath,
+					},
+				},
+			} as Options)
+		)
+	}
 
-    /**
-     * Run all SAO instances
-     */
+	/**
+	 * Run all SAO instances
+	 */
 
-    for (const sao of saoInstances) {
-        await runSao(sao, program)
-    }
+	for (const sao of saoInstances) {
+		await runSao(sao, program)
+	}
 }
 
 export default cli
